fdce82ac183a129a9d395ee2d351879fe36f92da
Mohammad Atiqur Rahman <atiqur15-6983@diu.edu.bd>
2021-10-04 17:34:47 +0600

    add components

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ShowServices from '../ShowServices/ShowServices';
 
 const Services = () => {
+    const [services,setServices] = useState([])
+    useEffect(()=>{
+        fetch('./services.JSON')
+        .then(res=>res.json())
+        .then(data=>setServices(data))
+    },[])
     return (
         <div>
-            <h1>take your services here!</h1>
+          <div className="container">
+          <div class="row row-cols-1 row-cols-md-4 g-4">
+            {
+                services.map(service=><ShowServices service={service}></ShowServices>)
+            }
+            </div>
+          </div>
         </div>
     );
 };
--- src/components/ShowCourses/ShowCourses.js
+++ src/components/ShowCourses/ShowCourses.js
@@ -6,13 +6,14 @@ const ShowCourses = (props) => {
     const {category,img,description,price,rating} = props.course;
     return (
         <div>
-        <div className="col card-height">
+        <div className="col">
             <div className="card h-100 text-start">
             <img src={img} className="card-img-top w-100 img-height" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{category}</h5>
                 <p className="card-text">{description.slice(0,150)}</p>
                 <p>Price: $ {price}</p>
+                <p>{rating}</p>
             </div>
             <div className="card-footer text-center text-white bg-dark">
                 <small>Enroll Now</small>
